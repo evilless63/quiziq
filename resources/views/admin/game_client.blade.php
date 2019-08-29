12162105592e235352572e9cdde5dd5223536e1f
vagrant <vagrant@homestead>
2019-08-29 15:41:52 +0000

    ручное редактирование позиции

--- resources/views/admin/game_client.blade.php
+++ resources/views/admin/game_client.blade.php
@@ -17,7 +17,12 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($game->totalscores()->orderBy('totalscore', 'desc')->orderBy('last_round_score', 'desc')->get() as $key=>$totalscore)
+            @if($game->use_alter_range == "1")
+                @php $coll = $game->totalscores()->orderBy('current_game_position', 'asc')->get() @endphp
+            @else
+                @php $coll = $game->totalscores()->orderBy('totalscore', 'desc')->get() @endphp
+            @endif
+            @foreach($coll as $key=>$totalscore)
             <tr @if( $game->is_over) @else style="display: none" @endif tabindex="{{count($game->teams()->get()->toArray()) - ($key + 1)}}">
                 <td class="game_number">{{$key + 1}}</td>
                 @if(isset($game->teams->where('id', $totalscore->team_id)->first()->ranks()->first()->image_path))
